6e5fe6d08c72fc342f831b95f908b5920bef33b7
Teebreezy101 <tammy.batubo@gmail.com>
2020-09-28 15:29:43 +0100

    add dynamic rendering of album info

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
+import { Injectable } from "@angular/core";
+import "rxjs/add/operator/map";
+import { Http, Response } from "@angular/http";
+import { map } from "rxjs/operator/map";
 
 @Injectable()
 export class ProductService {
+  private _albumUrl: string = "../assets/album.json";
 
-  constructor(private _http: Http) { }
-
+  constructor(private _http: Http) {
+    this._http = _http;
+  }
+  getAlbum(id: number) {
+    return this._http
+      .get(this._albumUrl)
+      .map((response: Response) => response.json());
+  }
 }
-
-
